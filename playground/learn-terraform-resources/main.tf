998f1ef9d633387de8827b303bc253bc7c030663
Chen-Han Hsiao (Stanley) <chenhan.hsiao.tw@gmail.com>
2021-09-04 07:08:25 +0800

    fmt

--- playground/learn-terraform-resources/main.tf
+++ playground/learn-terraform-resources/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "chenhan-terraform-playground"
-    key = "playground/learn-terraform-resources"
+    key     = "playground/learn-terraform-resources"
     profile = "chenhan"
     region  = "ap-northeast-1"
   }
@@ -13,9 +13,9 @@ provider "aws" {
 }
 
 locals {
-    terraform_path = "playground/playground"
-    instance_count                   = 1
-    instance_type                    = "t3a.nano"
+  terraform_path = "playground/playground"
+  instance_count = 1
+  instance_type  = "t3a.nano"
 }
 
 provider "random" {}
@@ -23,11 +23,11 @@ provider "random" {}
 resource "random_pet" "name" {}
 
 
-resource "aws_instance" "web" {  
-  ami                    = "ami-06cd52961ce9f0d85"  
-  instance_type          = local.instance_type
-  user_data              = file("init-script.sh")
-  tags = {    
+resource "aws_instance" "web" {
+  ami           = "ami-06cd52961ce9f0d85"
+  instance_type = local.instance_type
+  user_data     = file("init-script.sh")
+  tags = {
     Name = random_pet.name.id
-    }
+  }
 }
