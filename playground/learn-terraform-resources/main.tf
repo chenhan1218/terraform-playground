3043716021bf830e9eaa1c608d43dcb13b377c05
Chen-Han Hsiao (Stanley) <chenhan.hsiao.tw@gmail.com>
2021-11-13 15:14:10 +0800

    security group

--- playground/learn-terraform-resources/main.tf
+++ playground/learn-terraform-resources/main.tf
@@ -24,10 +24,27 @@ resource "random_pet" "name" {}
 
 
 resource "aws_instance" "web" {
-  ami           = "ami-06cd52961ce9f0d85"
-  instance_type = local.instance_type
-  user_data     = file("init-script.sh")
+  ami                    = "ami-06cd52961ce9f0d85"
+  instance_type          = local.instance_type
+  user_data              = file("init-script.sh")
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
   tags = {
     Name = random_pet.name.id
+  }
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.name.id}-sg"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
